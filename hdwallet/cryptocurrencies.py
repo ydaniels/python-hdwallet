20a42d4f2015f5335c786823e4f2fbddcaa28cfc
ydaniels <yomid4all@gmail.com>
2023-12-10 02:55:24 +0100

    Add ltc  support to generate p2wpkh address

--- hdwallet/cryptocurrencies.py
+++ hdwallet/cryptocurrencies.py
@@ -3452,18 +3452,18 @@ class LitecoinMainnet(Cryptocurrency):
     EXTENDED_PRIVATE_KEY = ExtendedPrivateKey({
         "P2PKH": 0x488ade4,
         "P2SH": 0x488ade4,
-        "P2WPKH": None,
-        "P2WPKH_IN_P2SH": None,
-        "P2WSH": None,
-        "P2WSH_IN_P2SH": None
+        "P2WPKH": 0x04b2430c,
+        "P2WPKH_IN_P2SH": 0x049d7878,
+        "P2WSH": 0x02aa7a99,
+        "P2WSH_IN_P2SH": 0x0295b005
     })
     EXTENDED_PUBLIC_KEY = ExtendedPublicKey({
         "P2PKH": 0x488b21e,
         "P2SH": 0x488b21e,
-        "P2WPKH": None,
-        "P2WPKH_IN_P2SH": None,
-        "P2WSH": None,
-        "P2WSH_IN_P2SH": None
+        "P2WPKH": 0x04b24746,
+        "P2WPKH_IN_P2SH": 0x049d7cb2,
+        "P2WSH": 0x02aa7ed3,
+        "P2WSH_IN_P2SH": 0x0295b43f
     })
 
     MESSAGE_PREFIX = "\x19Litecoin Signed Message:\n"
